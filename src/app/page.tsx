9e88196fa7cc8843e0307f86fa673de1debae696
rangelbjordao <96200914+rangelbjordao@users.noreply.github.com>
2025-03-05 15:33:27 -0300

    tela de inicio

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,101 +1,92 @@
-import Image from "next/image";
-
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+    return (
+        <>
+            {/* Título principal e descrição */}
+            <main>
+                <h1 className="my-2 text-center text-4xl font-bold">CCR Alertas</h1>
+                <p className="my-2 text-center">O programa de alertas para os funcionários da CCR</p>
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
-  );
+                {/* Seção de apresentação */}
+                <section className="flex flex-col items-center p-5 my-5 bg-[#999999] text-white rounded-lg shadow-md max-w-11/12 mx-auto w-full text-center">
+                    <h2 className="text-3xl font-semibold mb-4">Como CCR Alertas pode transformar as operações</h2>
+                    <p>O software CCR Alertas foi desenvolvido para melhorar a comunicação e a segurança operacional durante as
+                        operações, garantindo a transmissão rápida, eficiente e precisa de informações críticas,
+                        independentemente da localização ou das condições de trabalho. </p>
+                </section>
+
+                {/* Seção de vantagens */}
+                <section className="flex flex-col items-center p-5 my-5 bg-[#999999] text-white rounded-lg shadow-md max-w-11/12 mx-auto w-full text-center">
+
+                    <h2 className="text-3xl font-semibold mb-2">Vantagens</h2>
+
+                    <div className="flex flex-wrap gap-4 mt-2 justify-between">
+
+                        <div className="bg-white text-black rounded-lg shadow-md w-[calc(50%-10px)] p-4">
+                            <h3 className="mt-0 text-center text-xl font-semibold mb-4">Melhoria na Segurança</h3>
+
+                            <p>Com alertas imediatos, a segurança é priorizada, minimizando riscos e prevenindo acidentes.</p>
+                        </div>
+
+                        <div className="bg-white text-black rounded-lg shadow-md w-[calc(50%-10px)] p-4">
+                            <h3 className="mt-0 text-center text-xl font-semibold mb-4">Aumento da Eficiência</h3>
+
+                            <p>As funcionalidades de reportar eventos e solicitar ajuda melhoram a colaboração entre a equipe,
+                                aumentando a eficiência.</p>
+                        </div>
+
+                        <div className="bg-white text-black rounded-lg shadow-md w-[calc(50%-10px)] p-4">
+                            <h3 className="mt-0 text-center text-xl font-semibold mb-4">Registros para Análise de dados</h3>
+
+                            <p>Os alertas ficam registrados no histórico, facilitando a análise de dados e melhorando decisões
+                                futuras.</p>
+                        </div>
+
+                        <div className="bg-white text-black rounded-lg shadow-md w-[calc(50%-10px)] p-4">
+                            <h3 className="mt-0 text-center text-xl font-semibold mb-4">Uso em Ambientes Barulhentos</h3>
+
+                            <p>Em ambientes com muito ruído, é mais fácil visualizar um alerta do que escutar a comunicação pelo
+                                rádio.</p>
+                        </div>
+                    </div>
+                </section>
+
+                {/* Seção de funcionalidades */}
+                <section className="flex flex-col items-center p-5 my-5 bg-[#999999] text-white rounded-lg shadow-md max-w-11/12 mx-auto w-full text-center">
+
+                    <h2 className="text-3xl font-semibold mb-2">Funcionalidades</h2>
+
+                    <div className="flex flex-wrap gap-4 mt-2 justify-between">
+
+                        <div className="bg-white text-black rounded-lg shadow-md w-[calc(50%-10px)] p-4">
+                            <h3 className="mt-0 text-center text-xl font-semibold mb-4">Reportar Eventos</h3>
+
+                            <p>Permite que os funcionários gerem alertas sobre problemas nas estações e linhas, seguindo três
+                                etapas: Escolher a situação, o grupo que será destinado e definição do grau da gravidade.</p>
+                        </div>
+
+                        <div className="bg-white text-black rounded-lg shadow-md w-[calc(50%-10px)] p-4">
+                            <h3 className="mt-0 text-center text-xl font-semibold mb-4">Monitorar eventos em aberto</h3>
+
+                            <p>Permite que os funcionários acompanhem, em tempo real, todos os incidentes ou problemas que foram
+                                reportados e ainda estão pendentes de resolução.</p>
+                        </div>
+
+                        <div className="bg-white text-black rounded-lg shadow-md w-[calc(50%-10px)] p-4">
+                            <h3 className="mt-0 text-center text-xl font-semibold mb-4">Solicitar Ajuda</h3>
+
+                            <p>Permite que o funcionários solicitem ajuda para resolver um problema em aberto quando preciso,
+                                criando uma nova notificação para os funcionários do mesmo cargo.</p>
+                        </div>
+
+                        <div className="bg-white text-black rounded-lg shadow-md w-[calc(50%-10px)] p-4">
+                            <h3 className="mt-0 text-center text-xl font-semibold mb-4">Registro dos Alertas</h3>
+
+                            <p>O software mantém um histórico de alertas, fornecendo informações que facilitam a identificação de padrões e auxiliam na tomada de decisões para melhorias e solução de problemas.</p>
+                        </div>
+                    </div>
+                </section>
+
+            </main>
+        </>
+    );
 }
