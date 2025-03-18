c135f1f9fab686e9909586371a3c7ab0958564e2
rangelbjordao <96200914+rangelbjordao@users.noreply.github.com>
2025-03-18 08:52:25 -0300

    header fix

--- src/app/components/header/header.tsx
+++ src/app/components/header/header.tsx
@@ -5,14 +5,14 @@ export function Header() {
     return (
         <>
             {/* Cabeçalho da página */}
-            <header className="w-full bg-white py-6">
+            <header className="w-full bg-white py-6 sm: text-sm">
                 <div className="w-4xl max-w-11/12 mx-auto flex justify-between items-center">
                     <Link href="/" aria-label="Ir para a página inicial">
                         <Image src="/images/logo-ccr.png" alt="Logo da CCR" width={80} height={80} className="w-14 h-14 sm:w-16 sm:h-16 md:w-20 md:h-20 max-w-full max-h-full"></Image>
                     </Link>
 
                     {/* Menu de navegação */}
-                    <nav className="ml-8">
+                    <nav className="ml-8 ">
                         <ul className="flex list-none p-0">
                             <li>
                                 <Link className="text-black px-4 py-1 hover:underline" href="/">
